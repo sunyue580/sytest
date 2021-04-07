49ccb0fca135d04630b24f33889d7981823278b5
Yoricksy <2994271356@qq.com>
2021-04-07 23:11:18 +0800

    2021-04-07

---
+++ R/add2.R
@@ -0,0 +1,11 @@
+#' Add together two numbers.
+#'
+#' @param x A number
+#' @param y A number
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add2(1, 1)
+#' add2(10, 1)
+add2 <- function(x, y) {
+  x + y
+}
